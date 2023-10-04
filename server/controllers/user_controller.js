0f30d2a562267334b01460be530a3b7e06e1f6da
shreyanzjain <shreyanz32@gmail.com>
2023-10-04 12:36:30 +0530

    refactor userLogin

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,53 +1,73 @@
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+const cookieParser = require("cookie-parser");
+
 const SALT_ROUNDS = 10;
 
 const prisma = new PrismaClient();
 
-async function create_user(req, res) {
-    const {email, username, phone, password} = req.body;
-    const hashed_password = await bcrypt.hash(password, SALT_ROUNDS);
+export async function create_user(req, res) {
+  const { email, username, phone, password } = req.body;
+  const hashed_password = await bcrypt.hash(password, SALT_ROUNDS);
 
-    await prisma.user.create({
-        data: {
-            email: email,
-            username: username,
-            phone: phone,
-            hashed_password: hashed_password
-        }
+  await prisma.user
+    .create({
+      data: {
+        email: email,
+        username: username,
+        phone: phone,
+        hashed_password: hashed_password,
+      },
     })
-    .then(async()=>{
-        await prisma.$disconnect();
+    .then(async () => {
+      await prisma.$disconnect();
     })
     .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
     });
 }
 
 //User login
 export const userLogIn = async (req, res) => {
-    try {
-        const {username,password} = req.body;
-        let user = await User.findOne({ username: username});
-        if(!user){
-            return res.status(401).json({error:"Please enter correct Credential"})
-        }
+  try {
+    const { username, password } = req.body;
 
-        let passCompare = await bcrypt.compare(password,user.password);
-        if(!passCompare){
-            return res.status(401).json({error:"Please enter correct Credential"})
-        }
-        const data = {
-            user:{
-                id:user.id
-            }
-        }
-        const authtoken = jwt.sign(data, JWT_SECRET);
-        return res.status(200).json({authtoken});
-    } catch (error) {
-        res.status(500).json('Error: ', error.message);        
+    const user = await prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Please enter correct Credential" });
     }
-}
 
+    let passCompare = await bcrypt.compare(password, user.password);
+
+    if (!passCompare) {
+      return res.status(401).json({ error: "Please enter correct Credential" });
+    }
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    return res
+      .cookie("access_token", authtoken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "none",
+      })
+      .status(200)
+      .send("Login Successful");
+  } catch (error) {
+    res.status(500).json("Error: ", error.message);
+  }
+};
