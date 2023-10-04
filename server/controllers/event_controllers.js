4d5e28ce737e34d3f6dd1c781996da01138fd3e8
KrishnaKhadke <krishnakhadke11@gmail.com>
2023-10-04 16:42:37 +0530

    refactor

--- server/controllers/event_controllers.js
+++ server/controllers/event_controllers.js
@@ -4,15 +4,16 @@ const { fetchuser } = require("../middleware/fetchuser");
 const prisma = new PrismaClient();
 
 async function createEvent(req, res) {
-  const { title, desc, venue, time, organizer_id } = req.body;
-
+  const { title, desc, venue,startTime,endTime,imageUrl, organizer_id } = req.body;
   await prisma.event
     .create({
       data: {
         title: title,
         desc: desc,
         venue: venue,
-        time: parseInt(time),
+        startTime: parseInt(startTime),
+        endTime:parseInt(endTime),
+        imageUrl:imageUrl,
         organizer_id: organizer_id,
       },
     })
